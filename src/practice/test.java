4981285963edb3993c5e2126602dca54fdbdb860
Shova12 <prajapati.shova@gmail.com>
2017-07-17 08:04:39 -0400

    Update test.java
    add not

--- src/practice/test.java
+++ src/practice/test.java
@@ -5,7 +5,7 @@ public class test {
 		public static void main(String args[]) {
 			for(int i = 1; i < 3; i++){
 				  for(int j = 3; j > i; j--)
-				     assert i!=j; // complier error if there is ; symbol
+				     assert i!=j; // complier error if there is not ; symbol
 				  System.out.println(i);
 			}
 		}
